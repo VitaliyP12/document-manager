5301c59167a34877dcea345821cf91e06b091a6d
Vitaliy <v.perchuk04@gmail.com>
2026-05-02 16:01:27 +0300

    add database connection with Sequelize and PostgreSQL

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const sequelize = require('./config/database');
 
 const app = express();
 
@@ -13,5 +14,10 @@ app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.get('/', (req, res) => {
   res.json({ message: 'Document Manager API працює!' });
 });
+
+// Підключення до бази даних
+sequelize.authenticate()
+  .then(() => console.log('База даних підключена!'))
+  .catch(err => console.error('Помилка підключення до БД:', err));
 
 module.exports = app;
